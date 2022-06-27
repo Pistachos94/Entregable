521bcd77517563b05ea2e3d0bd5eb96d4ed1526e
Pistachos94 <106416986+Pistachos94@users.noreply.github.com>
2022-06-27 01:00:19 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 
 import './App.css'
-import Texto from './Modulos/Texto';
-import Bulb from './Modulos/Bombilla.jsx'
+
 import {useState} from 'react';
-import Cards from './Modulos/Cards';
+
 import Cardsquotes from './Modulos/Cardsquotes';
 
 function App() {
-  const [isOn, setIsOn] = useState(true);
 
-  const toggleIsOn = () => setIsOn(!isOn);
 
   return ( 
     <div className="App">
